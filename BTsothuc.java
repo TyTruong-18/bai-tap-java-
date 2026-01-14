113e26ceff2cdbe1e6e0c06bda667eeda934fd0d
TyTruong-18 <viipcucai@gmail.com>
2026-01-14 23:04:36 +0700

    Create BTsothuc.java

---
+++ BTsothuc.java
@@ -0,0 +1,15 @@
+package LEC5;
+
+import java.util.*;
+
+public class BTsothuc {
+
+    public static void main(String[] args) {
+        Random rd = new Random();
+        //double sothuc = rd.nextDouble()*(max-min)+min;
+        double sothuc = rd.nextDouble() * 6.2 + 1.8;
+        System.out.println("random: " + sothuc);
+
+    }
+
+}
